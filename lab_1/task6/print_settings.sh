d14f63cd63b1deab03ea84d204428bc22fc92761
sevak avetisyan <sevak.avet@gmail.com>
2014-11-11 01:11:26 +0400

    Renamed .sh file, changed permissions for conf. file

---
+++ lab_1/task6/print_settings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source settings.conf
+
+echo "########################################"
+echo "## Default login/password: $OS_USERNAME/$OS_PASSWORD"
+echo "## tenant: $OS_TENANT_NAME"
+echo "########################################"
