92ebb7de263ab59d1a2f7c1d9802b56ba5ead295
sevak avetisyan <sevak.avet@gmail.com>
2014-11-11 22:13:46 +0400

    Added #!/bin/bash

--- lab_1/task5/bashrc.sh
+++ lab_1/task5/bashrc.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 echo "User $USER logged into your system!" | mail -s "Login to Ubuntu" sevak.avet@gmail.com
--- lab_1/task5/startup.sh
+++ lab_1/task5/startup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cat bashrc.sh >> ~/.bashrc # При логине через su
 cat sshrc >> /etc/ssh/sshrc # Для отправки почты при заходе по ssh
 cp bashrc.sh /etc/init.d # При загрузке системы
