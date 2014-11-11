041fd37ecde70ea45c95aea5da4cbdbde03c67ab
sevak avetisyan <sevak.avet@gmail.com>
2014-11-11 23:01:35 +0400

    Fixed task3: using awk

--- lab_2/t_3.sh
+++ lab_2/t_3.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-count=0
-
-ps aux | while read line; do
-  arr=($line)
-  let "count += 1"
-
-  if [ "${count}" -eq 1 ]; then
-    echo "----------------------------------------------------------------"
-    printf "%s\t%-20s\t%s\n" "USER" "COMAND" "%CPU"
-    echo "----------------------------------------------------------------"
-  elif [ $(echo "if (${arr[2]} > 0.0) 1 else 0" | bc) -eq "1" ]; then
-    printf "%s\t%-20s\t%.1f\n" ${arr[0]} $(basename ${arr[10]}) ${arr[2]}
-  fi
-done
+echo "------------------------------------------"
+printf "%s\t%-20s\t%s\n" "USER" "COMAND" "%CPU"
+echo "------------------------------------------"
+ps aux | awk '{if ($3 > 0.0) printf "%s\t%-20s\t%.1f\n", $1, basename($11), $3}
+				function basename(pn){sub(/^.*\//, "", pn); return pn}'
