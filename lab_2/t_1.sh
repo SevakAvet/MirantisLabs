9954a8af6d9b165757bc437d18ba33e446233f76
sevak avetisyan <sevak.avet@gmail.com>
2014-11-12 00:20:31 +0400

    Finished task1

--- lab_2/t_1.sh
+++ lab_2/t_1.sh
@@ -3,13 +3,9 @@
 export filename_pattern="./[a-zA-Z]*"
 export text_pattern="[a-zA-Z]"
 
-find . -type f -regex ${filename_pattern}
-
 path_to_dirs=""
 while read line; do
   path_to_dirs="${path_to_dirs} ${line}* "
-done < dir_lst_file
+done < ${dir_lst_file}
 
-grep -R ${text_pattern} $path_to_dirs
-
-// Как совместить grep и find?
+find . -type f -regex ${filename_pattern} | xargs grep -R ${text_pattern} ${path_to_dirs}
